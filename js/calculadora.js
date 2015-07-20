fa2123de64044973eee1b2c27cc2b669d7320774
yobal31 <yonicristhbl@hotmail.com>
2015-07-20 17:41:19 -0500

    ultimo

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,14 +3,23 @@
 var a,b,c,d;
 
 $(document).ready(function(e) {
+	$("#number_num_1").numeric();
+	$("#number_num_2").numeric();
+	$("#number_den_1").numeric();
+	$("#number_den_2").numeric();
+	
 	$("#bt_start, #bt_new_calc").click(function(e) {
 		NewCalc();
     });
-    $("#bt_suma").click(function(e) {
-        Operar(1);
+    $("#bt_suma,#bt_resta,#bt_multiplica,#bt_divide").click(function(e) {
+        Resultado($(this).data("oper"));
 		$.mobile.changePage("#pg_Result", { transition: "slide", changeHash: false });
     });
-	$("#bt_close_app").click(function(e) {
+	$("#bt_suma_r,#bt_resta_r,#bt_multiplica_r,#bt_divide_r").click(function(e) {
+        Resultado($(this).data("oper"));
+		//$.mobile.changePage("#pg_Result", { transition: "slide", changeHash: false });
+    });
+	$("#bt_close_app,#bt_close_app2,#bt_close_app3").click(function(e) {
         navigator.app.exitApp();
     });
 });
@@ -21,54 +30,116 @@ function NewCalc() {
 	c=0;
 	d=0;
 	
-	$("#number_num_1").val(0);
-	$("#number_num_2").val(0);
-	$("#number_den_1").val(0);
-	$("#number_den_2").val(0);
+	$("#number_num_1").val("");
+	$("#number_num_2").val("");
+	$("#number_den_1").val("");
+	$("#number_den_2").val("");
 	
 	$.mobile.changePage("#pg_Calc", { transition: "slide", changeHash: false });	
 }
 
+function Resultado(operador) {
+	var r = new Array(4);
+	r=Operar(operador);
+	
+	var sim = new Array(2);
+	sim = Simplificar(r[0],r[1]);
+	
+	$("#sp_signo").text(operador);
+	
+	$("#sp_num_1").text(a);
+	$("#sp_num_2").text(c);
+	
+	$("#sp_den_1").text(b);
+	$("#sp_den_2").text(d);
+	
+	$("#sp_num_3").text(r[2]);
+	$("#sp_den_3").text(r[3]);
+	
+	$("#sp_num_4").text(r[0]);
+	$("#sp_den_4").text(r[1]);
+	
+	$("#sp_num_simp,#sp_num_simp2").text(sim[0]);
+	$("#sp_den_simp,#sp_den_simp2").text(sim[1]);
+}
+
 function Operar(operador){
-	var resultado=new Array(2);
+	var resultado=new Array(4);
 	
-	a= $("#number_num_1").val();
-	c= $("#number_num_2").val();
+	Validar();
 	
-	b= $("#number_den_1").val();
-	d= $("#number_den_2").val();
+	a= parseInt($("#number_num_1").val());
+	c= parseInt($("#number_num_2").val());
+	
+	b= parseInt($("#number_den_1").val());
+	d= parseInt($("#number_den_2").val());
 	
 	var EqualDen = false;
 	
 	if (b==d) { EqualDen=true; } //mismo denominador
-	
+		
 	switch (operador) {
-		case 2: //resta
+		case "-": //resta
 			if (EqualDen==true) {
 				resultado[0]=a-c;
 				resultado[1]=b;
+				resultado[2]=a+" - "+c;
+				resultado[3]=b;
 			} else {
 				resultado[0]=(a*d)-(b*c);
 				resultado[1]=b*d;
+				resultado[2]="("+a+" x "+d+")"+" - "+"("+b+" x "+c+")";
+				resultado[3]=b+" x "+d;
 			}
 			break;
-		case 3: //multiplica
+		case "x": //multiplica
 			resultado[0]=a*c;
 			resultado[1]=b*d;
+			resultado[2]=a+" x "+c;
+			resultado[3]=b+" x "+d;
 			break;
-		case 4: //divide
+		case "/": //divide
 			resultado[0]=a*d;
 			resultado[1]=b*c;
+			resultado[2]=a+" x "+d;
+			resultado[3]=b+" x "+c;
 			break;
 		default: //suma			
 			if (EqualDen==true) {
 				resultado[0]=a+c;
 				resultado[1]=b;
+				resultado[2]=a+" + "+c;
+				resultado[3]=b;
 			} else {
 				resultado[0]=(a*d)+(b*c);
 				resultado[1]=b*d;
+				resultado[2]="("+a+" x "+d+")"+" + "+"("+b+" x "+c+")";
+				resultado[3]=b+" x "+d;
 			}			
 			break;
 	}
 	return resultado;
+}
+
+function Validar() {
+	if($("#number_den_1").val()==0) {$("#number_den_1").val(1)}
+	if($("#number_den_2").val()==0) {$("#number_den_2").val(1)}
+}
+
+function Simplificar(a,b) {
+	 var M = mcd(a,b);
+	 var sim = new Array(2);
+	 sim[0]=a;sim[1]=b;
+	 sim[0] /=M;
+	 sim[1] /=M;
+	 return sim;
+}
+
+function mcd(a,b) {
+	var r = a % b;
+	if(r==0) {
+	  return b;
+	 } else {
+	  return mcd(b,r);
+	 }
 }

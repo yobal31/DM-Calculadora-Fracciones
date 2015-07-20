8417a97dd1f830a57ccac1ad43fe8128391250b0
yobal31 <yonicristhbl@hotmail.com>
2015-07-20 13:02:34 -0500

    suma

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,12 +1,33 @@
 // JavaScript Document
 
+var a,b,c,d;
+
 $(document).ready(function(e) {
+	$("#bt_start, #bt_new_calc").click(function(e) {
+		NewCalc();
+    });
     $("#bt_suma").click(function(e) {
         Operar(1);
+		$.mobile.changePage("#pg_Result", { transition: "slide", changeHash: false });
+    });
+	$("#bt_close_app").click(function(e) {
+        navigator.app.exitApp();
     });
 });
 
-var a,b,c,d;
+function NewCalc() {
+	a=0;
+	b=0;
+	c=0;
+	d=0;
+	
+	$("#number_num_1").val(0);
+	$("#number_num_2").val(0);
+	$("#number_den_1").val(0);
+	$("#number_den_2").val(0);
+	
+	$.mobile.changePage("#pg_Calc", { transition: "slide", changeHash: false });	
+}
 
 function Operar(operador){
 	var resultado=new Array(2);
@@ -32,10 +53,12 @@ function Operar(operador){
 			}
 			break;
 		case 3: //multiplica
-		
+			resultado[0]=a*c;
+			resultado[1]=b*d;
 			break;
 		case 4: //divide
-		
+			resultado[0]=a*d;
+			resultado[1]=b*c;
 			break;
 		default: //suma			
 			if (EqualDen==true) {
